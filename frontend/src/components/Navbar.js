f9e48309493c47b5118c56d1b8dd33c78805892e
Hamza Ahmed <ahmedhamza884@github.com>
2022-09-15 00:51:53 +0500

    logged in previligies

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { UserContext } from "../App";
 export default function Navbar() {
+  const { state, dispatch } = useContext(UserContext);
+  const renderList = () => {
+    if (state) {
+      return [
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li>
+          <Link to="/createPost">Create Post</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li>
+          <Link to="/login">Login</Link>
+        </li>,
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>,
+      ];
+    }
+  };
   return (
     <nav>
       <div className="nav-wrapper white" style={{ color: "black" }}>
-        <Link to="/" className="brand-logo left">
+        <Link to={state ? "/" : "/login"} className="brand-logo left">
           Instagram
         </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/createPost">Create Post</Link>
-          </li>
+          {renderList()}
         </ul>
       </div>
     </nav>
